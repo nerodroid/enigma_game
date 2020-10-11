df7f52d2210a476134fd624ce1ed241b0253688f
AvishkaWeebadde <senithweebadde@gmail.com>
2020-10-11 06:18:09 +0530

    pause menu added to blackSmith scene

---
+++ Assets/pauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                resume();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+
+    public void resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void loadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("QUIT!"); 
+        Application.Quit();
+    }
+}
