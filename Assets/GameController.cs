df38ee34ee2f46b016219528e893edcd4e885940
kavindyaS007 <kavindyasamarasinghe@gmail.com>
2020-11-24 16:03:28 +0530

    level 1 testing

---
+++ Assets/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public void GetInput(string guess){
+		//Debug.Log ("You entered " + guess);
+		if(string.Equals(guess,"123")){
+			Debug.Log ("level completed");
+		}
+		else{
+			Debug.Log ("try again");
+		}
+	}
+}
---
+++ Assets/guessAnswer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guessAnswer : MonoBehaviour
+{
+    //public static bool GameIsPaused = false;
+    public GameObject guessKeyUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            guessKeyUI.SetActive(true);
+			//Time.timeScale = 1f;		
+        }
+		if(Input.GetKeyDown(KeyCode.E))
+        {
+            guessKeyUI.SetActive(false);
+			//Time.timeScale = 1f;		
+        }
+    }
+}
